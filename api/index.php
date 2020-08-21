00c2f7e22e23ec77ca94cccc7d36bb097beacb89
Nick <noobping@users.noreply.github.com>
2020-08-21 19:42:09 +0200

    forgot to save it

--- api/index.php
+++ api/index.php
@@ -66,7 +66,7 @@ class App
             array_shift($this->param);
             $arr = explode('/', $tmp);
             $class = "\\endpoint\\" . ucfirst($arr[0]);
-            array_merge((array)$this->param, (array)$arr);
+            $this->param = array_merge((array)$this->param, (array)$arr);
         }
 
         $this->controller = new $class(); // specify the controller (endpoint)
