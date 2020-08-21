88bae973ee1f59a212402ce64e27690a4560ad16
Nick <noobping@users.noreply.github.com>
2020-08-21 21:59:07 +0200

    rewrite to vuetify

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,116 +1,70 @@
 <template>
-  <v-skeleton-loader class="mt-5 mx-auto" type="card" :loading="loading">
-    <Scroll />
+  <div>
+    <v-skeleton-loader class="mt-5 mx-auto" type="card" :loading="loading">
+      <Scroll />
 
-    <Plotly
-      id="day"
-      :data="power"
-      :layout="{
-        title: day.TotalYield + ' Wh in ' + day.TimeStamp,
-        xaxis: { fixedrange: true },
-        yaxis: { fixedrange: true },
-      }"
-      :display-mode-bar="false"
-      :autoResize="true"
-    ></Plotly>
-    <v-btn pill block color="primary" v-on:click="add()">
-      <v-icon>mdi-plus</v-icon>Add
-    </v-btn>
-  </v-skeleton-loader>
+      <v-card flat>
+        <v-card-title> {{ today / 1000 }} kWh </v-card-title>
+        <v-sparkline
+          :value="power"
+          color="primary"
+          stroke-linecap="round"
+          smooth
+          fill
+          auto-draw
+        >
+        </v-sparkline>
+      </v-card>
+    </v-skeleton-loader>
+
+    <Date type="date" :loading="loading"></Date>
+  </div>
 </template>
 
 <script>
 /*! This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA. */
 
-import axios from "axios";
 import store from "@/store";
-import { Plotly } from "vue-plotly";
 
 export default {
   name: "Day",
   components: {
-    Plotly,
     Scroll: () => import("@/components/ScrollToBottom"),
+    Date: () => import("@/components/Date"),
   },
   data: () => ({
-    page: new Date(),
+    menu: false,
   }),
-  created() {
-    this.page.setDate(this.page.getDate() - 1);
-  },
   computed: {
+    date: {
+      get() {
+        return store.state.app.date;
+      },
+      set(n) {
+        store.commit("date", n);
+        store.commit("loadingByKey", "day");
+        store.dispatch("get", { key: "day", arg: n });
+      },
+    },
+    power() {
+      return store.state.api.day.map((e) => {
+        return parseInt(e.Power);
+      });
+    },
+    today() {
+      return store.state.api.month.length > 0 &&
+        undefined !== store.state.api.month[0].DayYield
+        ? store.state.api.month[0].DayYield
+        : 0;
+    },
     loading() {
       return store.getters.isLoadingByKey("day");
     },
-    day() {
-      return store.state.api.day.length > 0 &&
-        undefined !== store.state.api.day[0].TimeStamp &&
-        undefined !== store.state.api.day[0].TotalYield
-        ? store.state.api.day[0]
-        : {
-            TimeStamp: new Date(),
-            TotalYield: 0,
-          };
-    },
-    power() {
-      return [
-        {
-          x:
-            store.state.api.day.length > 0
-              ? store.state.api.day.map(function (o) {
-                  return undefined !== o.TimeStamp
-                    ? o.TimeStamp
-                    : new Date().getTime();
-                })
-              : [new Date().getTime()],
-          y:
-            store.state.api.day.length > 0
-              ? store.state.api.day.map(function (o) {
-                  return undefined !== o.Power ? o.Power : 0;
-                })
-              : [0],
-          type: "scatter",
-          line: {
-            color: "#1976d2",
-          },
-        },
-      ];
-    },
   },
   watch: {
+    power() {},
+    today() {},
     loading() {},
-  },
-  methods: {
-    add() {
-      store.commit("loadingByKey", "day");
-      const url =
-        (store.state.app.url
-          ? (store.state.app.secure ? "https://" : "http://") +
-            store.state.app.url
-          : "") +
-        "/api/?r=day/" +
-        this.page.toISOString().substring(0, 10);
-      if (store.state.api.debug) console.log("GET", url);
-
-      if (store.state.api.day.length == 0) this.page = new Date();
-
-      axios
-        .get(url, {
-          crossDomain: store.state.app.url ? true : false,
-        })
-        .then((res) => {
-          if (res.status == 200 && res.data.error)
-            console.error(res.data.error.message, res.data.error);
-          if (res.status == 200 && res.data.day) {
-            this.page.setDate(this.page.getDate() - 1);
-            store.commit("add", {
-              key: "day",
-              value: res.data.day,
-            });
-          }
-        })
-        .catch(() => store.commit("show_dialog"));
-    },
   },
 };
 </script>
--- src/components/Month.vue
+++ src/components/Month.vue
@@ -1,113 +1,58 @@
 <template>
-  <v-skeleton-loader class="mt-5 mx-auto" type="card" :loading="loading">
-    <Scroll />
+  <div>
+    <v-skeleton-loader class="mt-5 mx-auto" type="card" :loading="loading">
+      <Scroll></Scroll>
 
-    <Plotly
-      id="month"
-      :data="months"
-      :layout="{
-        title: begin.TotalYield + ' Wh in ' + begin.TimeStamp,
-        xaxis: { fixedrange: true },
-        yaxis: { fixedrange: true },
-      }"
-      :display-mode-bar="false"
-      :autoResize="true"
-    ></Plotly>
-    <v-btn pill block color="primary" v-on:click="add()">
-      <v-icon>mdi-plus</v-icon>Add
-    </v-btn>
-  </v-skeleton-loader>
+      <v-card flat>
+        <v-card-title> {{ today }} </v-card-title>
+        <v-sparkline
+          :value="power"
+          :gradient="['#1976d2']"
+          type="bar"
+          smooth
+          fill
+          auto-line-width
+          auto-draw
+        >
+        </v-sparkline>
+      </v-card>
+    </v-skeleton-loader>
+
+    <Date type="month" :loading="loading"></Date>
+  </div>
 </template>
 
 <script>
 /*! This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA. */
 
-import axios from "axios";
 import store from "@/store";
-import { Plotly } from "vue-plotly";
 
 export default {
   name: "Month",
   components: {
-    Plotly,
     Scroll: () => import("@/components/ScrollToBottom"),
-  },
-  data: () => ({
-    page: new Date(),
-  }),
-  created() {
-    this.page.setMonth(this.page.getMonth() - 1);
+    Date: () => import("@/components/Date"),
   },
   computed: {
+    power() {
+      return store.state.api.month.map((e) => {
+        return parseInt(e.DayYield);
+      });
+    },
     loading() {
       return store.getters.isLoadingByKey("month");
     },
-    begin() {
-      return store.state.api.month[0]
-        ? store.state.api.month[0]
-        : {
-            TotalYield: 0,
-            TimeStamp: new Date(),
-          };
-    },
-    months() {
-      return [
-        {
-          x:
-            store.state.api.month.length > 0
-              ? store.state.api.month.map(function (o) {
-                  return undefined !== o.TimeStamp ? o.TimeStamp : new Date();
-                })
-              : [new Date()],
-          y:
-            store.state.api.month.length > 0
-              ? store.state.api.month.map(function (o) {
-                  return undefined !== o.DayYield ? o.DayYield : 0;
-                })
-              : [0],
-          type: "bar",
-          marker: {
-            color: "#1976d2",
-          },
-        },
-      ];
+    today() {
+      return store.state.api.month.length > 0 &&
+        undefined !== store.state.api.month[0].TotalYield
+        ? store.state.api.month[0].TotalYield
+        : 0;
     },
   },
   watch: {
+    power() {},
     loading() {},
-    begin() {},
-  },
-  methods: {
-    add() {
-      store.commit("loadingByKey", "month");
-      const url =
-        (store.state.app.url
-          ? (store.state.app.secure ? "https://" : "http://") +
-            store.state.app.url
-          : "") +
-        "/api/?r=month/" +
-        this.page.toISOString().substring(0, 10);
-      if (store.state.api.debug) console.log("GET", url);
-
-      if (store.state.api.month.length == 0) this.page = new Date();
-
-      axios
-        .get(url, {
-          crossDomain: store.state.app.url ? true : false,
-        })
-        .then((res) => {
-          if (res.status == 200 && res.data.error)
-            console.error(res.data.error.message, res.data.error);
-          if (res.status == 200 && res.data.month) {
-            this.page.setMonth(this.page.getMonth() - 1);
-            store.commit("add", {
-              key: "month",
-              value: res.data.month,
-            });
-          }
-        })
-        .catch(() => store.commit("show_dialog"));
-    },
+    today() {},
   },
 };
 </script>
